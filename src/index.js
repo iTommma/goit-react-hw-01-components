3cfb25b3ab9c4f3bab834ded970473be8054a577
iTommma <itommma@gmail.com>
2023-02-06 19:12:48 +0200

    profile

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import profileData from './user.json'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App profile={profileData} />
   </React.StrictMode>
 );
